70274f976d4329e1da8ca017c7735870a10e360f
Junhao Wang <junhaowanggg@gmail.com>
2024-03-24 11:10:48 -0700

    Set up vertex binding and descriptions

--- core/typedefs.h
+++ core/typedefs.h
@@ -28,6 +28,24 @@ using F32 = float;
 #endif
 using F64 = double;
 
+// Math
+#include <glm/glm.hpp>
+using Vec1 = glm::vec1;
+using Vec2 = glm::vec2;
+using Vec3 = glm::vec3;
+using Vec4 = glm::vec4;
+
+using Mat2x2 = glm::mat2x2;
+using Mat3x3 = glm::mat3x3;
+using Mat4x4 = glm::mat4x4;
+
+using Mat2x3 = glm::mat2x3;
+using Mat2x4 = glm::mat2x4;
+using Mat3x4 = glm::mat3x4;
+using Mat4x3 = glm::mat4x3;
+using Mat4x2 = glm::mat4x2;
+using Mat3x2 = glm::mat3x2;
+
 // Reference
 template <typename T>
 using Scope = std::unique_ptr<T>;
--- tutorial/hello_triangle.cpp
+++ tutorial/hello_triangle.cpp
@@ -11,6 +11,40 @@
 static const U32 kWidth = 800;
 static const U32 kHeight = 600;
 
+struct Vertex
+{
+	Vec2 position;
+	Vec3 color;
+
+	static VkVertexInputBindingDescription GetBindingDescription()
+	{
+		VkVertexInputBindingDescription bindingDescription{};
+		bindingDescription.binding = 0;
+		bindingDescription.stride = sizeof(Vertex);
+		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+		return bindingDescription;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> GetAttributeDescriptions()
+	{
+		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+		return attributeDescriptions;
+	}
+};
+
+static const std::vector<Vertex> kVertexData = { { { 0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f } },
+												 { { 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f } },
+												 { { -0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f } } };
+
 static const std::vector<const char*> kValidationLayers = { "VK_LAYER_KHRONOS_validation" };
 static const std::vector<const char*> kDeviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME,
 															"VK_KHR_portability_subset" };
@@ -574,10 +608,14 @@ void HelloTriangleApplication::CreateGraphicsPipeline()
 	// Vertex input format
 	VkPipelineVertexInputStateCreateInfo vertexInputCreateInfo{};
 	vertexInputCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-	vertexInputCreateInfo.vertexBindingDescriptionCount = 0;	 // currently hard coded in vertex shader
-	vertexInputCreateInfo.pVertexBindingDescriptions = nullptr;	 // Optional
-	vertexInputCreateInfo.vertexAttributeDescriptionCount = 0;
-	vertexInputCreateInfo.pVertexAttributeDescriptions = nullptr;  // Optional
+
+	auto bindingDescription = Vertex::GetBindingDescription();
+	vertexInputCreateInfo.vertexBindingDescriptionCount = 1;
+	vertexInputCreateInfo.pVertexBindingDescriptions = &bindingDescription;
+
+	auto attributeDescription = Vertex::GetAttributeDescriptions();
+	vertexInputCreateInfo.vertexAttributeDescriptionCount = static_cast<U32>(attributeDescription.size());
+	vertexInputCreateInfo.pVertexAttributeDescriptions = attributeDescription.data();
 
 	// Input assembly
 	VkPipelineInputAssemblyStateCreateInfo inputAssemblyCreateInfo{};
